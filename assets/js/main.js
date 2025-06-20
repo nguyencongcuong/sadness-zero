7ceecf753c1e3603c9fa24404be6496fca107630
Cuong Nguyen <c.ng.devie@icloud.com>
2025-06-20 14:36:27 +0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,119 +65,120 @@ fetchData("./assets/json/constellation.json", planetName);
 // 3.1 Intro
 // 3.2 Healing process
 // 3.3 Outro
-const intro = () => {
-    section1.style.display = "flex";
-    let wait = async () => {
-        await delay(3000);
-        section1.classList.remove("intro--fadeIn");
-        section1.classList.add("intro--fadeOut");
-        await delay(3000);
-        section1.style.display = "none";
-        section2.style.display = "flex";
-        section2.classList.add("healing--fadeIn");
-    }
-    wait();
-}
-const process = () => {
+// const intro = () => {
+//     section1.style.display = "flex";
+//     let wait = async () => {
+//         await delay(3000);
+//         section1.classList.remove("intro--fadeIn");
+//         section1.classList.add("intro--fadeOut");
+//         await delay(3000);
+//         section1.style.display = "none";
+//         section2.style.display = "flex";
+//         section2.classList.add("healing--fadeIn");
+//     }
+//     wait();
+// }
+// const process = () => {
 
-    // Variables
-    let healingWords = document.getElementById("healingWords");
-    let spaceStar = document.getElementById("spaceStar");
-    let thoughts = document.getElementById("thoughts");
-    let sharingField = document.getElementById("sharingField");
-    let input = document.getElementById("thoughtsInput");
-    let inputValue = "";
-    let healingWordList = [
-        "Bạn ơi, hãy thư giãn và thả lỏng cơ thể...",
-        "hít một hơi thật sâu... rồi sau đó thở ra.",
-        "Bầu trời có lúc mưa, lúc đen xì vì bão tố...",
-        "nhưng cuối cùng, trời lại quang mây lại tạnh.",
-        "Cuộc sống của bạn cũng vậy...",
-        "Dù hiện bạn đang buồn thế nào thì nó đều trở về hình hài tốt đẹp vốn dĩ.",
-        "Vũ trụ rộng hơn 93 tỷ năm ánh sáng.",
-        "Bầu trời mênh mông, nhưng so với vũ trụ lại cực kỳ nhỏ bé.",
-        "Trái đất nơi bạn ở, lại còn nhỏ hơn thế nữa.",
-        "Những thành phố lớn nhất chỉ như 1 hạt bụi khi so với vũ trụ.",
-        "Còn bạn, chỉ cỡ như 1 tế bào.",
-        "Nỗi buồn trong bạn nhỏ bé đến nỗi...",
-        "Nó có thể tan biến trong không gian một cách dễ dàng..."
-    ]
+//     // Variables
+//     let healingWords = document.getElementById("healingWords");
+//     let spaceStar = document.getElementById("spaceStar");
+//     let thoughts = document.getElementById("thoughts");
+//     let sharingField = document.getElementById("sharingField");
+//     let input = document.getElementById("thoughtsInput");
+//     let inputValue = "";
+//     let healingWordList = [
+//         "Bạn ơi, hãy thư giãn và thả lỏng cơ thể...",
+//         "hít một hơi thật sâu... rồi sau đó thở ra.",
+//         "Bầu trời có lúc mưa, lúc đen xì vì bão tố...",
+//         "nhưng cuối cùng, trời lại quang mây lại tạnh.",
+//         "Cuộc sống của bạn cũng vậy...",
+//         "Dù hiện bạn đang buồn thế nào thì nó đều trở về hình hài tốt đẹp vốn dĩ.",
+//         "Vũ trụ rộng hơn 93 tỷ năm ánh sáng.",
+//         "Bầu trời mênh mông, nhưng so với vũ trụ lại cực kỳ nhỏ bé.",
+//         "Trái đất nơi bạn ở, lại còn nhỏ hơn thế nữa.",
+//         "Những thành phố lớn nhất chỉ như 1 hạt bụi khi so với vũ trụ.",
+//         "Còn bạn, chỉ cỡ như 1 tế bào.",
+//         "Nỗi buồn trong bạn nhỏ bé đến nỗi...",
+//         "Nó có thể tan biến trong không gian một cách dễ dàng..."
+//     ]
 
-    // Hide #sharingField
-    sharingField.style.display = "none";
+//     // Hide #sharingField
+//     sharingField.style.display = "none";
 
-    // Get the input value & assign it to #thoughts
-    inputValue = input.value;
-    thoughts.innerText = inputValue;
+//     // Get the input value & assign it to #thoughts
+//     inputValue = input.value;
+//     thoughts.innerText = inputValue;
 
-    // Based on healingWordList.length, caculate showing time for each healing words in 60s
-    let timeToWait = 60000 / healingWordList.length;
+//     // Based on healingWordList.length, caculate showing time for each healing words in 60s
+//     let timeToWait = 60000 / healingWordList.length;
 
-    // Execute in order
-    let wait = async () => {
+//     // Execute in order
+//     let wait = async () => {
 
-        // Make #spaceStar disappear gradually in 60s
-        spaceStar.classList.add("healing__star--disappear");
+//         // Make #spaceStar disappear gradually in 60s
+//         spaceStar.classList.add("healing__star--disappear");
 
-        // While the star is disappearing, displaying healing words in order
-        for (let i = 0; i < healingWordList.length; i++) {
-            healingWords.innerText = healingWordList[i];
-            await delay(timeToWait); // how long each sentence displays on screen
-            if (i == healingWordList.length - 1) {
-                healingWords.classList.add("healing__words--fadeOut");
-                await delay(3000);
-                section2.style.display = "none";
-            }
-        }
-    }
+//         // While the star is disappearing, displaying healing words in order
+//         for (let i = 0; i < healingWordList.length; i++) {
+//             healingWords.innerText = healingWordList[i];
+//             await delay(timeToWait); // how long each sentence displays on screen
+//             if (i == healingWordList.length - 1) {
+//                 healingWords.classList.add("healing__words--fadeOut");
+//                 await delay(3000);
+//                 section2.style.display = "none";
+//             }
+//         }
+//     }
 
-    wait();
+//     wait();
 
-}
-const outro = () => {
-    // Execute in order
-    let wait = async () => {
-        await delay(62000); // Wait for the flow of #section2 to finish
-        section3.style.display = "flex";
-        section3.classList.add("outro--fadeIn");
-        await delay(15000);
-        section3.classList.add("outro--fadeOut");
-        await delay(4000);
-        section3.style.display = "none";
-    }
-    wait();
-}
-const audio = () => {
+// }
+// const outro = () => {
+//     // Execute in order
+//     let wait = async () => {
+//         await delay(62000); // Wait for the flow of #section2 to finish
+//         section3.style.display = "flex";
+//         section3.classList.add("outro--fadeIn");
+//         await delay(15000);
+//         section3.classList.add("outro--fadeOut");
+//         await delay(4000);
+//         section3.style.display = "none";
+//     }
+//     wait();
+// }
+// const audio = () => {
 
-    // Variables
-    let music;
-    let randomAudioIndex;
+//     // Variables
+//     let music;
+//     let randomAudioIndex;
 
-    // Fetch audios from API
-    fetch("./assets/json/bg-music.json")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (audioList) {
-            // Choose an random audio file
-            randomAudioIndex = randomRange(audioList.length - 1, 0);
-            music = new Audio(audioList[randomAudioIndex].url);
-            // Loop audio
-            if (typeof music.loop == 'boolean') {
-                music.loop = true;
-            } else {
-                music.addEventListener('ended', function () {
-                    this.currentTime = 0;
-                    this.play();
-                }, false);
-            }
-            // Play audio
-            music.play();
-            console.log("Bạn đang nghe bản nhạc " + audioList[randomAudioIndex].name + " - " + audioList[randomAudioIndex].author);
-        });
-}
+//     // Fetch audios from API
+//     fetch("./assets/json/bg-music.json")
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (audioList) {
+//             // Choose an random audio file
+//             randomAudioIndex = randomRange(audioList.length - 1, 0);
+//             music = new Audio(audioList[randomAudioIndex].url);
+//             // Loop audio
+//             if (typeof music.loop == 'boolean') {
+//                 music.loop = true;
+//             } else {
+//                 music.addEventListener('ended', function () {
+//                     this.currentTime = 0;
+//                     this.play();
+//                 }, false);
+//             }
+//             // Play audio
+//             music.play();
+//             console.log("Bạn đang nghe bản nhạc " + audioList[randomAudioIndex].name + " - " + audioList[randomAudioIndex].author);
+//         });
+// }
 
 intro();
+
 document.getElementById("submitBtn").addEventListener("click", function (event) {
     event.preventDefault(); // Prevent broswer from reload after click
     audio();
